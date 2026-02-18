4efae74eb03b9790f504145a5904f4d3063f6965
Section33-ops <saeedwumbei33@gmail.com>
2026-02-18 06:41:10 -0500

    - Create a function called getComputerChoice - Get a random number with Math.random and store it in randomChoice variable - Use the random number to decide whether rock, paper ort scissors - Then store it in randomChoice and return it to the function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 console.log("Hello, World");
+
+function getComputerChoice() {
+  let randomChoice = Math.random();
+  if (randomChoice < 0.3) {
+    randomChoice = "rock";
+  } else if (randomChoice > 0.33 && randomChoice < 0.6) {
+    randomChoice = "paper";
+  } else if (randomChoice > 0.66) {
+    randomChoice = "scissors";
+  }
+
+  return randomChoice;
+}
+
+console.log(getComputerChoice());
